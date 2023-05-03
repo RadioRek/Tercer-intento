6389b330b79ee0bcd6b4fe86a77f4e09af327330
RadioRek <nic.ortega@duocuc.cl>
2023-05-03 18:51:55 -0400

    mas cosas random

--- Script.js
+++ Script.js
@@ -18,4 +18,51 @@ function updateClock() {
 }
 
 // Call updateClock every second
+updateClock();
 setInterval(updateClock, 1000);
+
+// adress form function
+$(document).ready(function () {
+    $('#confirmButton').click(function () {
+        let email = $('#inputEmail').val();
+        let adress = $('#inputAddress').val();
+        let city = $('#inputCity').val();
+        let state = $('#inputState').val();;
+        let postalCode = $('#inputPostal').val();;
+
+        if ($.trim(email) === "") {
+            $('#inputEmail').addClass('notValid');
+            event.preventDefault();
+        } else {
+            $('#inputEmail').removeClass('notValid');
+        }
+
+        if ($.trim(adress) === "") {
+            $('#inputAddress').addClass('notValid');
+            event.preventDefault();
+        } else {
+            $('#inputAddress').removeClass('notValid');
+        }
+
+        if ($.trim(city) === "") {
+            $('#inputCity').addClass('notValid');
+            event.preventDefault();
+        } else {
+            $('#inputCity').removeClass('notValid');
+        }
+
+        if (state === "Choose...") {
+            $('#inputState').addClass('notValid');
+            event.preventDefault();
+        } else {
+            $('#inputState').removeClass('notValid');
+        }
+
+        if ($.trim(postalCode) === "") {
+            $('#inputPostal').addClass('notValid');
+            event.preventDefault();
+        } else {
+            $('#inputPostal').removeClass('notValid');
+        }
+    });
+});
