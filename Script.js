54c00768e997b3d568faa99247bec1bb375b4da7
RadioRek <nic.ortega@duocuc.cl>
2023-05-12 17:10:16 -0400

    cambios locos

--- Script.js
+++ Script.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
         let adress = $('#inputAddress').val();
         let city = $('#inputCity').val();
         let state = $('#inputState').val();;
-        let postalCode = $('#inputPostal').val();;
+        let postalCode = $('#inputPostal').val();
 
         if ($.trim(email) === "") {
             $('#inputEmail').addClass('notValid');
@@ -71,69 +71,66 @@ $(document).ready(function () {
         $(".myCardBlack").css("animation", "1.5s linear 0s infinite cardRotate");
         $(".myCardGrey").css("animation", "1.5s linear 0s infinite cardRotate");
 
-    })
+    });
 
     $('#danceButton').mouseup(function () {
 
         $(".myCardBlack").css("animation", "");
         $(".myCardGrey").css("animation", "");
 
-    })
+    });
 
+    $('#lightButton').click(function () {
+
+        if ($('.card').hasClass('bg-dark') || $('.card').hasClass('text-bg-dark')) {
+            $('.card').removeClass('bg-dark').addClass('bg-light');
+            $('.card').removeClass('text-bg-dark').addClass('text-bg-light');
+        } else {
+            $('.card').removeClass('bg-light').addClass('bg-dark');
+            $('.card').removeClass('text-bg-light').addClass('text-bg-dark');
+        }
+
+        if ($('.tittle').hasClass('bg-dark') || $('.tittle').hasClass('text-bg-dark')) {
+            $('.tittle').removeClass('bg-dark').addClass('bg-light');
+            $('.tittle').removeClass('text-bg-dark').addClass('text-bg-light');
+        } else {
+            $('.tittle').removeClass('bg-light').addClass('bg-dark');
+            $('.tittle').removeClass('text-bg-light').addClass('text-bg-dark');
+        }
+
+        if ($("#myFooter").hasClass("bg-dark")) {
+            $("#myFooter").removeClass("bg-dark").addClass("bg-light");
+            $("#myFooter").removeClass("text-bg-dark").addClass("text-bg-light");
+        } else {
+            $("#myFooter").removeClass("bg-light").addClass("bg-dark");
+            $("#myFooter").removeClass("text-bg-light").addClass("text-bg-dark");
+        }
+
+        if ($('#myNavBar').attr('data-bs-theme') === 'dark') {
+            $('#myNavBar').attr('data-bs-theme', 'light');
+        } else {
+            $('#myNavBar').attr('data-bs-theme', 'dark');
+        }
+
+        if ($(".mySidebar").hasClass("text-bg-dark")) {
+            $(".mySidebar").removeClass("bg-dark").addClass("bg-light");
+            $(".mySidebar").removeClass("text-bg-dark").addClass("text-bg-light");
+        } else {
+            $(".mySidebar").removeClass("bg-light").addClass("bg-dark");
+            $(".mySidebar").removeClass("text-bg-light").addClass("text-bg-dark");
+        }
+
+        if ($(".sidelink").hasClass("text-bg-dark")) {
+            $(".sidelink").removeClass("bg-dark").addClass("bg-light");
+            $(".sidelink").removeClass("text-bg-dark").addClass("text-bg-light");
+        } else {
+            $(".sidelink").removeClass("bg-light").addClass("bg-dark");
+            $(".sidelink").removeClass("text-bg-light").addClass("text-bg-dark");
+        }
+    });
+
+    
 });
-
-const newDivRow = document.createElement('div');
-newDivRow.classList.add('row');
-newDivRow.classList.add('justify-content-center');
-newDivRow.classList.add('bg-dark');
-newDivRow.classList.add('mx-0');
-newDivRow.classList.add('py-3');
-
-document.getElementById('userContainer').appendChild(newDivRow);
-
-
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((data) => data.forEach(element => {
-
-        const newDivCol = document.createElement('div');
-        newDivCol.classList.add('col-auto');
-
-        newDivRow.appendChild(newDivCol);
-
-        const newDivCard = document.createElement('div');
-        newDivCard.classList.add('card');
-        newDivCard.classList.add('bg-secondary');
-        newDivCard.classList.add('my-3');
-        newDivCard.style.width = '18rem';
-
-        newDivCol.appendChild(newDivCard);
-
-        const newDivCardBody = document.createElement('div');
-        newDivCardBody.classList.add('card-body');
-
-        newDivCard.appendChild(newDivCardBody);
-
-        const newH5 = document.createElement('h5');
-        newH5.classList.add('card-title');
-        newH5.classList.add('text-bg-secondary');
-        newH5.innerHTML = element.name;
-
-        newDivCardBody.appendChild(newH5);
-
-        const newH6 = document.createElement('h6');
-        newH6.classList.add('text-bg-secondary');
-        newH6.innerHTML = element.email;
-
-        newDivCardBody.appendChild(newH6);
-
-        const newh6_2 = document.createElement('h6');
-        newh6_2.classList.add('text-bg-secondary');
-        newh6_2.innerHTML = element.phone;
-
-        newDivCardBody.appendChild(newh6_2);
-    }))
-    .catch((error) => console.error(error));
 
 
 
